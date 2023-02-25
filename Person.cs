a6f75394206b822c5a2e73e063aa61e18cced37b
jahon1507 <User@USER-PC>
2023-02-25 13:14:09 +0500

    init

---
+++ Person.cs
@@ -0,0 +1,12 @@
+﻿namespace MyLib
+{
+    public class Person
+    {
+        string name;
+        public Person(string name) 
+        {
+            this.name = name;
+        }
+        public void Print() => Console.WriteLine($"Name: {name}");
+    }
+}
